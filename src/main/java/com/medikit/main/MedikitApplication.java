ad972b816812ea4a39e69e79f414cce4bc49f15b
lalit <lalit@DESKTOP-077QKLL>
2021-02-19 14:51:05 +0530

    first step

---
+++ src/main/java/com/medikit/main/MedikitApplication.java
@@ -0,0 +1,13 @@
+package com.medikit.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedikitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedikitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medikit/main/MedikitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medikit.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedikitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
